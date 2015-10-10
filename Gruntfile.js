7506b91d0cb403cc61e0fc96f5c10fae2d111105
Jared Moore <JMoore@dealersocket.com>
2015-10-09 19:37:49 -0600

    Working prototype

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,67 @@
 module.exports = function (grunt) {
 
-  // Force use of Unix newlines
-  //grunt.util.linefeed = '\n';
+    // Force use of Unix newlines
+    //grunt.util.linefeed = '\n';
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    less: {
-      main: {
-        options: {
-          strictMath: false,
-          sourceMap: true,
-          outputSourceFiles: false,
-          sourceMapURL: 'app.css.map',
-          sourceMapFilename: 'app/assets/css/app.css.map'
+        less: {
+            main: {
+                options: {
+                    strictMath: false,
+                    sourceMap: true,
+                    outputSourceFiles: false,
+                    sourceMapURL: 'app.css.map',
+                    sourceMapFilename: 'app/assets/css/app.css.map'
+                },
+                files: [{
+                    'app/assets/css/main.css': 'app/assets/**/*.less'
+                },
+                    {
+                        'styles/main.css': 'styles/**/*.less'
+                    }]
+            }
         },
-        files: [{
-          'app/assets/css/assets.css': 'app/assets/less/*.less'
+        watch: {
+            options: {
+                liveReload: true
+            },
+            styles: {
+                // Which files to watch (all .modules files)
+                files: ['styles/**/*.less', 'app/assets/**/*.less'],
+                tasks: ['less', 'concat','cssmin'],
+                options: {
+                    nospawn: true
+                }
+            }
         },
-          {
-            'styles/styles.css': 'styles/less/*.less'
-          },
-          {
-            'app.css': '*.less'
-          }]
-      }
-    },
-    watch: {
-      options: {
-        liveReload: true
-      },
-      styles: {
-        // Which files to watch (all .less files)
-        files: ['styles/**/*.less', 'app/assets/less/**/*.less'],
-        tasks: ['less'],
-        options: {
-          nospawn: true
+
+        cssmin: {
+            options: {
+                //shorthandCompacting: false,
+                //roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'app.min.css': 'app.css'
+                }
+            }
+        },
+        concat: {
+            basic: {
+                src: ['app/assets/css/main.css', 'styles/main.css'],
+                dest: 'app.css'
+            }
         }
-      }
-    },
+    });
 
-    cssmin: {
-      options: {
-        //shorthandCompacting: false,
-        //roundingPrecision: -1
-      },
-      target: {
-        files: {
-          'app/assets/css/app.css': ['app/assets/css/app.min.css']
-        }
-      }
-    },
-    concat: {
-      basic: {
-        src: ['app/assets/css/*.css','styles/css/*.css','app.css'],
-        dest: 'compiled.css'
-      }
-    }
-  });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  //grunt.loadNpmTasks('grunt-contrib-copy');
-  //grunt.loadNpmTasks('grunt-contrib-clean');
-
-  // Default task(s).
-  grunt.registerTask('default', ['less','concat']);
+    // Default task(s).
+    grunt.registerTask('default', ['less', 'concat','cssmin']);
 };
--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,32 @@
 /**
  * Created by Xenox on 10/8/2015.
  */
-//angular.module('styleguide',[]);
-//angular.module('styleguide').controller('')
+var app = angular.module('styleguide', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'app/views/home.html'
+    })
+        .when('/Item1', {
+            templateUrl: 'app/views/Item1.html'
+        })
+        .when('/Item2', {
+            templateUrl: 'app/views/Item2.html'
+        })
+        .when('/Item3',{
+            templateUrl: 'app/views/Item3.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}]);
+angular.module('styleguide').controller('navController', ["$location",function ($location) {
+
+    this.component = function(){
+        var template = (''+$location.path()).substring(1);
+        if(template)
+            return template;
+        else{
+            return "Welcome!"
+        }
+    }
+}]);
